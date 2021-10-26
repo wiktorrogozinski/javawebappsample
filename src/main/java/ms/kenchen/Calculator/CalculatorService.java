0e8052cd54dec3d4916c87b8cf7b3e006d6ac285
wiktorrogozinski <62955170+wiktorrogozinski@users.noreply.github.com>
2021-10-26 19:59:47 +0200

    Update CalculatorService.java

--- src/main/java/ms/kenchen/Calculator/CalculatorService.java
+++ src/main/java/ms/kenchen/Calculator/CalculatorService.java
@@ -13,7 +13,7 @@ public class CalculatorService {
     @Path("ping")
     @Produces(MediaType.TEXT_PLAIN)
     public String ping() {
-        return "Welcome to Java Web App - malpa na torowisku v2! This is updated v2!\n" + new Date().toString();
+        return "Welcome to Wiktor Java Web App! This is updated v5 - Sicario commit!\n" + new Date().toString();
     }
 
     @GET
