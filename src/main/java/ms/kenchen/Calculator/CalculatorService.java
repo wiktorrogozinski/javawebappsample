f37081e2b124386fa59b2d4711e3a0f7784f53e1
wiktor_rogozinski <wiktor.rogozinski@wp.pl>
2021-11-01 19:32:34 +0100

    dsl

--- src/main/java/ms/kenchen/Calculator/CalculatorService.java
+++ src/main/java/ms/kenchen/Calculator/CalculatorService.java
@@ -13,7 +13,7 @@ public class CalculatorService {
     @Path("ping")
     @Produces(MediaType.TEXT_PLAIN)
     public String ping() {
-        return "Welcome to Wiktor Java Web App! This is updated v14 - Sicario commit + dsl!\n" + new Date().toString();
+        return "Welcome to Wiktor Java Web App! This is updated v15 - Sicario commit + dsl!\n" + new Date().toString();
     }
 
     @GET
